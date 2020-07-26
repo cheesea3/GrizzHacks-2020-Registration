ab6f28a7dfd77cfd8666654b46baa5e3aad9995a
daiyaan <3193289+cheesea3@users.noreply.github.com>
2020-07-25 22:58:40 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Daiyaan Ijaz">
+    <title>Starter Template · Bootstrap</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+
+    <!-- Inline CSS -->
+    <style>
+
+    </style>
+    <!-- Custom styles -->
+    <link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top" style="border-bottom: 1px solid #000000">
+    <a class="navbar-brand" href="index.php">Grizzhacks 5</a>
+</nav>
+
+<div class="container">
+        <h1> starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+
+
+</div><!-- /.container -->
+
+
+
+
+
+
+
+
+
+
+
+
+<footer class="footer mt-auto py-3" style="padding-top: 20px; display:block; position:relative; bottom:0; border-top: 1px solid #225378; margin-bottom:0;">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-3"><span class="text-muted lead"><h6>The GrizzHacks student team is passionate about technology and organizing engaging events on Oakland University's campus. Contact us to know more or to get involved!</h6></span></div>
+        </div>
+    </div>
+</footer>
+
+
+</body>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+<!--- to top -->
+<script src="js/main.js"></script>
+
+</html>
